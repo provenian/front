236ac083f408a9ff6d926d74d5c3b0c0f7805bec
myuon <ioi.joi.koi.loi@gmail.com>
2019-09-09 22:09:31 +0900

    design: support small screen (responsive)

--- src/view/Index.tsx
+++ src/view/Index.tsx
@@ -1,33 +1,35 @@
 import React from "react";
-import { Header, Image, Grid } from "semantic-ui-react";
+import { Header, Image, Grid, Container } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 
 const Index: React.FC = () => {
   return (
-    <Grid centered>
-      <Grid.Row>
-        <Grid.Column width={8}>
-          <Header as="h2">ようこそ</Header>
-          <Image src={`${process.env.PUBLIC_URL}/top_neko.jpg`} />
-        </Grid.Column>
-      </Grid.Row>
+    <Container text>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column>
+            <Header as="h2">ようこそ</Header>
+            <Image src={`${process.env.PUBLIC_URL}/top_neko.jpg`} />
+          </Grid.Column>
+        </Grid.Row>
 
-      <Grid.Row>
-        <Grid.Column width={8}>
-          <Header as="h2">Provenianとは</Header>
-          <p>
-            Provenianは定理証明オンラインジャッジサイトです。証明問題を掲載しています。連絡等は
-            <a href="https://twitter.com/myuon_myon">@myuon_myon</a>
-            までお願いします。
-          </p>
+        <Grid.Row>
+          <Grid.Column>
+            <Header as="h2">Provenianとは</Header>
+            <p>
+              Provenianは定理証明オンラインジャッジサイトです。証明問題を掲載しています。連絡等は
+              <a href="https://twitter.com/myuon_myon">@myuon_myon</a>
+              までお願いします。
+            </p>
 
-          <p>
-            初めての方や言語ごとの環境などについて知りたい方は
-            <Link to="/how-to-use">問題の解き方</Link>のページをご覧ください。
-          </p>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+            <p>
+              初めての方や言語ごとの環境などについて知りたい方は
+              <Link to="/how-to-use">問題の解き方</Link>のページをご覧ください。
+            </p>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </Container>
   );
 };
 
